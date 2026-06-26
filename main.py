b2ae7e93a3fea72800c2aaef315eb2daf58fd33b
Riad Mahmud Chowdhury <129725417+riadc916@users.noreply.github.com>
2026-06-26 23:29:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if today in birthday_dict:
         contents=contents.replace("[NAME]",birthday_person["name"])
 
     with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
-        connection.login(user=my_email,password=password)
-        connection.sendmail(from_addr=my_email,to_addrs=birthday_person["email"],msg=f"Subject: Happy Birthday!\n\n{contents}")
+        connection.login(user=MY_EMAIL,password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,to_addrs=birthday_person["email"],msg=f"Subject: Happy Birthday!\n\n{contents}")
 
 
